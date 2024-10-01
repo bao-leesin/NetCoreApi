73b83bfcc13942fa68df211e313f16b86b34b645
baohdt <baohd@vtc.vn>
2024-10-01 13:39:49 +0700

    refactor

--- Program.cs
+++ Program.cs
@@ -1,23 +1,7 @@
-using Microsoft.EntityFrameworkCore;
-using NetCoreApi.Entities;
-using Serilog;
 var builder = WebApplication.CreateBuilder(args);
 
-
-//Add logger
-Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo.File(
-    "Logs/testlog.txt",
-    outputTemplate: "[{Timestamp:HH:mm:ss dd:MM:yyyy.fff.zzz} {Level:u3}] {Message:lj}{NewLine}{Exception}",
-    rollingInterval: RollingInterval.Month
-    ).CreateLogger();
-builder.Host.UseSerilog();
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DbContextCore>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Hometown"));
-
-});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
